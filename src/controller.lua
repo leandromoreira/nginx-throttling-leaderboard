addb37229ae73085467a36b7b546fa9ac213375a
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2022-02-15 12:19:54 -0300

    refactors code for better usage

--- src/controller.lua
+++ src/controller.lua
@@ -1,30 +1,81 @@
+-- this driver is included into openresty
+-- https://github.com/openresty/lua-resty-redis
 local redis = require "resty.redis"
 local leaderboard = require "leaderboard"
 
 local controller = {}
 
+local redis_host = "redis"
+local redis_port = 6379
+local expected_concurrent_users = 10000
+local connect_timeout = 20 -- ms
+local send_timeout = 20 -- ms
+local read_timeout = 20 -- ms
+local max_idle_timeout = 5000 -- ms (connection pool timeout)
+
 controller.start_redis_client = function()
-  local redis_client = redis:new()
-  redis_client:set_timeouts(1000, 1000, 1000)
-  local _, _ = redis_client:connect("redis", 6379)
-  return redis_client
+  local pool_size = expected_concurrent_users / ngx.worker.count()
+  local redis_client, err = redis:new()
+  if err ~= nil then
+    return nil, err
+  end
+
+  redis_client:set_timeouts(connect_timeout, send_timeout, read_timeout)
+  local _, err = redis_client:connect(redis_host, redis_port, {pool_size=pool_size})
+
+  if err ~= nil then
+    return nil, err
+  end
+
+  return redis_client, nil
 end
 
 controller.finish_redis_client = function(redis_client)
-  local _, _ = redis_client:set_keepalive(10000, 100)
+  local pool_size = expected_concurrent_users / ngx.worker.count()
+  return redis_client:set_keepalive(max_idle_timeout, pool_size)
 end
 
-controller.hit = function(token)
-  local redis_client = controller.start_redis_client()
-  leaderboard.hit(redis_client, token)
-  controller.finish_redis_client(redis_client)
+controller.token_hit = function()
+  local redis_client, err = controller.start_redis_client()
+  if err ~= nil then
+    ngx.log(ngx.ERR, err)
+    return
+  end
+
+  local _, err = leaderboard.hit(redis_client, ngx.var.arg_token)
+  if err ~= nil then
+    ngx.log(ngx.ERR, err)
+    return
+  end
+
+  local _, err = controller.finish_redis_client(redis_client)
+  if err ~= nil then
+    ngx.log(ngx.ERR, err)
+    return
+  end
 end
 
-controller.top = function(quantity)
-  local redis_client = controller.start_redis_client()
-  local results = leaderboard.top(redis_client, quantity)
-  controller.finish_redis_client(redis_client)
-  return results
+controller.render_top_used_tokens = function()
+  local redis_client, err = controller.start_redis_client()
+  if err ~= nil then
+    ngx.log(ngx.ERR, err)
+    return
+  end
+
+  local json_response, err = leaderboard.top(redis_client, ngx.var.arg_quantity or 10)
+  if err ~= nil then
+    ngx.log(ngx.ERR, err)
+    return
+  end
+
+  ngx.header.content_type = "application/json"
+  ngx.say(json_response)
+
+  local _, err = controller.finish_redis_client(redis_client)
+  if err ~= nil then
+    ngx.log(ngx.ERR, err)
+    return
+  end
 end
 
 return controller
--- src/leaderboard.lua
+++ src/leaderboard.lua
@@ -1,31 +1,71 @@
 local leaderboard = {}
 
-local key_prefix = "leaderboard"
+local key_prefix = "leaderboard_"
 local math_floor = math.floor
 local ngx_now = ngx.now
+local ttl = 2 * 60 + 30 -- 2.5s
+
+leaderboard.key_prefix = key_prefix
+leaderboard.ttl = ttl
+leaderboard.now = ngx_now
+
+leaderboard._current_minute = function()
+  return math_floor((leaderboard.now() / 60) % 60)
+end
+
+leaderboard._current_key = function()
+  return leaderboard.key_prefix .. leaderboard._current_minute()
+end
+
+leaderboard._past_minute = function()
+  return (leaderboard._current_minute() + 59) % 60
+end
+
+leaderboard._past_key = function()
+  return leaderboard.key_prefix .. leaderboard._past_minute()
+end
 
 leaderboard.hit = function(redis_client, token)
-  local current_key = key_prefix .. math_floor((ngx_now() / 60) % 60)
+  if redis_client == nil or token == nil or token == "" then
+    return nil, "you must inform the redis client and a proper token"
+  end
 
   redis_client:init_pipeline()
-  redis_client:expire(current_key, 60)
-  redis_client:zincrby(current_key, 1, token)
+  redis_client:expire(leaderboard._current_key(), leaderboard.ttl) -- set expire time for current key
+  redis_client:zincrby(leaderboard._current_key(), 1, token) -- increment token usage each time
 
   local resp, err = redis_client:commit_pipeline()
   if err then
     return nil, err
   end
 
-  return resp[1]
+  return resp, nil
 end
 
 leaderboard.top = function(redis_client, quantity)
-  local current_key = key_prefix .. math_floor((ngx_now() / 60) % 60)
+  if redis_client == nil then
+    return nil, "you must inform the redis client"
+  end
   quantity = quantity or 10
 
-  local resp, err = redis_client:zrange(current_key, 0, quantity - 1, "REV", "WITHSCORES")
+  local root_response = {}
+
+  root_response["current_minute"]=leaderboard._current_minute()
+  local response, _ = leaderboard._top(redis_client, leaderboard._current_key(), quantity)
+  root_response["current_ranking"] = response
+
+  root_response["past_minute"]=leaderboard._past_minute()
+  response, _ = leaderboard._top(redis_client, leaderboard._past_key(), quantity)
+  root_response["past_ranking"] = response
+
+  return require "cjson".encode(root_response)
+end
+
+leaderboard._top = function(redis_client, key, quantity)
+  local resp, err = redis_client:zrange(key, 0, quantity - 1, "REV", "WITHSCORES")
+
   if err then
-    return nil, err
+    return {}, err
   end
 
   local total_items = #resp
@@ -38,7 +78,7 @@ leaderboard.top = function(redis_client, quantity)
     i = i + 2
   end
 
-  return require "cjson".encode(response)
+  return response, nil
 end
 
 return leaderboard
